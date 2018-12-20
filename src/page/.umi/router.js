3221429c48f7e305b950e6236860110ddacb9029
webliyan <itliyan@sina.cn>
2018-12-20 11:33:40 +0800

    1.搭建react项目

---
+++ src/page/.umi/router.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/_renderRoutes';
+
+
+let Router = DefaultRouter;
+
+let routes = [
+  {
+    "path": "/",
+    "component": require('../HelloWorld').default,
+    "exact": true
+  },
+  {
+    "component": () => React.createElement(require('D:/react/antd-course/node_modules/_umi-build-dev@1.2.7@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/page', hasRoutesInConfig: true })
+  }
+];
+window.g_plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+export default function() {
+  return (
+<Router history={window.g_history}>
+      { renderRoutes(routes, {}) }
+    </Router>
+  );
+}
